8164fac2f52445d3b0adff68418159e98c48269a
Drake-Estilette <drakeestilette516@gmail.com>
2025-09-25 00:19:30 -0600

    added three entries into pokedex index

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-alert("Hello World");
-let favoriteFood = "Beans and Rice";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = pokedexNumber1{
+    name: "Bulbasaur",
+    height: 0.7,
+    type: "Grass/Poison"
+};
+pokemonList[1] = pokedexNumber2{
+    name: "Ivysaur",
+    height: 1.0,
+    type: "Grass/Poison"
+};
+pokemonList[2] = pokedexNumber3{
+    name: "Venusaur",
+    height: 2.0,
+    type: "Grass/Poison"
+};

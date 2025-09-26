65ff047969fe47486744fcfdaec330ba25c1c449
Drake-Estilette <drakeestilette516@gmail.com>
2025-09-26 10:46:37 -0600

    Removed object names in pokemonlist array, making them anonymous objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [];
-pokemonList[0] = pokedexNumber1{
-    name: "Bulbasaur",
-    height: 0.7,
-    type: "Grass/Poison"
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 0.7,
+  type: "Grass/Poison",
 };
-pokemonList[1] = pokedexNumber2{
-    name: "Ivysaur",
-    height: 1.0,
-    type: "Grass/Poison"
+pokemonList[1] = {
+  name: "Ivysaur",
+  height: 1.0,
+  type: "Grass/Poison",
 };
-pokemonList[2] = pokedexNumber3{
-    name: "Venusaur",
-    height: 2.0,
-    type: "Grass/Poison"
+pokemonList[2] = {
+  name: "Venusaur",
+  height: 2.0,
+  type: "Grass/Poison",
 };

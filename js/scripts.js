28020bf7bed6a187b90fb0abf08bd9f891c72d6c
Drake-Estilette <drakeestilette516@gmail.com>
2025-09-26 10:59:19 -0600

    added a for loop that prints the current array index onto the screen, and a conditional checking the height to see if it is larger than 1 meter

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList = [];
+// Should change this to be interactive in the future
 pokemonList[0] = {
   name: "Bulbasaur",
   height: 0.7,
@@ -14,3 +15,15 @@ pokemonList[2] = {
   height: 2.0,
   type: "Grass/Poison",
 };
+// ... add more Pokémon as needed
+
+// Loop through the array and print each Pokémon's name
+for (let ind = 0; ind < pokemonList.length; ind++) {
+  // Check if the height is greater than 1 meter
+  if (pokemonList[ind].height > 1) {
+    // If true, print the name with a special message
+    document.write(pokemonList[ind].name + " - Wow that's big!" + "<br>");
+  } else {
+    document.write(pokemonList[ind].name + "<br>");
+  }
+}
